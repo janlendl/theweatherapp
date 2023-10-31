256f18f4b2442e5105eb7af15a5fc9a2c7e76b8f
Lendl Peralta <janlendl@gmail.com>
2023-10-31 10:55:45 -0600

    Add state for search component. Add sample array

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,6 +1,17 @@
-/import React from 'react'
+import React, { useState} from 'react'
 
 export default function Search() {
+
+  const [searchCity, setSearchCity] = useState('');
+
+  const cities = [
+    {name: 'Calgary', province: 'Alberta'},
+    {name: 'Airdie', province: 'Alberta'},
+    {name: 'Toronto', province: 'Ontario'},
+    {name: 'Vancouver', province: 'British Columbia'},
+    {name: 'Whistler', province: 'British Columbia'},
+    {name: 'Moncton', province: 'New Brunswick'},
+  ]
   return (
     <label>
       <input name='citySearch' defaultValue='Enter City'/>
