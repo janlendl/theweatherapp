60f25aea1c799c8316203bafe9549bfcfc196e0d
Lendl Peralta <janlendl@gmail.com>
2023-11-08 13:18:30 -0700

    Create weather info with app.js passing data to child (weather info) as props

--- src/App.js
+++ src/App.js
@@ -1,34 +1,32 @@
-import React, { useState } from 'react';
-import Header from './components/Header';
-// import WeatherInfo from './components/WeatherInfo';
-import './App.css';
+import React, { useState } from "react";
+import Header from "./components/Header";
+import WeatherInfo from "./components/WeatherInfo";
+import "./App.css";
 
 export default function App() {
-  
-  const [searchCity, setSearchCity] = useState('');
-  
+  const [searchCity, setSearchCity] = useState("");
+
   const handleChange = (e) => {
     setSearchCity(e.target.value);
-  }
+  };
 
   return (
     <>
-    <Header/>
-    <div>
-      <input
-        type='text'
-        placeholder='Enter City'
-        onChange={handleChange}
-        value={searchCity}
-      />
-      {/* <button></button>*/}
+      <Header />
       <div>
+        <input
+          type="text"
+          placeholder="Enter City"
+          onChange={handleChange}
+          value={searchCity}
+        />
+        {/* <button></button>*/}
+        {/* <div>
         <h3>City is: {searchCity}</h3>
+      </div> */}
       </div>
-    </div>
 
+      <WeatherInfo city={searchCity}/>
     </>
-
-  )
+  );
 }
-
---
+++ src/components/WeatherInfo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function WeatherInfo(props) {
+  return (
+    <div>
+      <h2>City selected: {props.city}</h2>
+    </div>
+  )
+}
