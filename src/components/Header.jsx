26b0adb2892fe57a43385479de0547c5c35524e5
Lendl Peralta <janlendl@gmail.com>
2023-11-08 13:49:47 -0700

    Fix dependencies. Create Header sass file

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
+import './Header.scss';
 
-export default function Banner() {
+export default function Header() {
   return (
     <div className="homeBanner">
     <h2 className="banner">How's the weather today?</h2>
