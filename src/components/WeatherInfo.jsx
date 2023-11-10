1ccc8ec6e8f194a3f95265a4f57492ff279749ba
Lendl Peralta <janlendl@gmail.com>
2023-11-10 14:01:40 -0700

    Add usestate for weather result

--- src/components/WeatherInfo.jsx
+++ src/components/WeatherInfo.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import axios from 'axios';
 
 export default function WeatherInfo(props) {
+
+  const [weatherResult, setweatherResult] = useState([]);
+
+  let weatherData = {
+    method: 'GET',
+    url: `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=743366476b7670d23d2788f10c157d96`,
+  }
+
+  
   return (
     <div>
       <h2>City selected: {props.city}</h2>
