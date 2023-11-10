5fec3c5097aa557a1fed11b68122e3aa4c67fb21
Lendl Peralta <janlendl@gmail.com>
2023-11-10 14:20:08 -0700

    Add axios GET request

--- src/components/WeatherInfo.jsx
+++ src/components/WeatherInfo.jsx
@@ -2,15 +2,27 @@ import React, {useEffect, useState} from 'react'
 import axios from 'axios';
 
 export default function WeatherInfo(props) {
+  const {city} = props;
+  console.log('::CITY:: ', city);
 
   const [weatherResult, setweatherResult] = useState([]);
 
   let weatherData = {
     method: 'GET',
-    url: `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=743366476b7670d23d2788f10c157d96`,
+    url: `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=743366476b7670d23d2788f10c157d96`,
   }
 
-  
+  useEffect(() => {
+    axios.request(weatherData)
+      .then((res) => {
+        console.log('Weather API call:: ',res.data);
+      })
+      .catch((err) => {
+        console.log('Error: ', err);
+      });
+  }, )
+    
+
   return (
     <div>
       <h2>City selected: {props.city}</h2>
