cf6ffd2ea59a05e6a356418f7e5a2bace32b2d9c
Lendl Peralta <janlendl@gmail.com>
2023-11-15 09:56:03 -0700

    Moved url out of a function and changed axios to a get instead of a request

--- src/components/WeatherInfo.jsx
+++ src/components/WeatherInfo.jsx
@@ -5,29 +5,27 @@ export default function WeatherInfo(props) {
   //const {city} = props;
   console.log('::CITY:: ', props.city);
 
-  const [weatherResult, setWeatherResult] = useState([{}]);
-
-  let weatherData = {
-    method: 'GET',
-    url: `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=743366476b7670d23d2788f10c157d96`,
-  }
-
+  const [weatherResult, setWeatherResult] = useState({});
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=743366476b7670d23d2788f10c157d96`;
+  
   useEffect(() => {
-    axios.request(weatherData)
+    if (props.city) {
+      axios.get(url)
       .then((res) => {
         setWeatherResult(res.data);
-        console.log('Weather API call:: ',weatherResult);
+        console.log('Weather API call:: ', res.data);
       })
       .catch((err) => {
         console.log('Error: ', err);
       });
-  },[props.city]);
+    }
+  },[props.city, url]);
     
-
   return (
     <div>
       <h2>City selected: {props.city}</h2>
       <h3>Weather Today is</h3>
+      {weatherResult.name}
     </div>
   )
 }
