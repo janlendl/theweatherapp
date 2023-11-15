0627b0e95b0597e296c94e93875a51624050b0a6
Lendl Peralta <janlendl@gmail.com>
2023-11-15 13:19:53 -0700

    Uploading to repo for more troubleshooting

--- src/components/WeatherInfo.jsx
+++ src/components/WeatherInfo.jsx
@@ -7,11 +7,12 @@ export default function WeatherInfo(props) {
   const [weatherData, setWeatherData] = useState({});
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=743366476b7670d23d2788f10c157d96`;
   
+  // connect to weather API
   useEffect(() => {
     if (props.city) {
       axios.get(url)
       .then((res) => {
-        JSON.stringify(setWeatherData(res.data));
+        setWeatherData(res.data);
         console.log('Weather API call:: ', res.data);
       })
       .catch((err) => {
@@ -20,31 +21,35 @@ export default function WeatherInfo(props) {
     }
   },[props.city, url]);
 
-  const weatherDetails = weatherData.map((weather) => {
+  // pass weather details to Weather Card component
+  const weatherDetails = Object.keys(weatherData).map((data) => {
+    console.log('::::WEATHER:::: ', data.weather[0].description);
+    
     return (
-      <WeatherCard>
-        key={weather.id}
-        description={weather.description}
-        feelsLike={weather.main.feels_like}
-        temp={weather.main.temp}
-        min={weather.main.temp_min}
-        max={weather.main.temp_max}
+      <WeatherCard
+        // key={weather.id}
+        // description={weather[0].description}
+        // feelsLike={weather.main.feels_like}
+        // temp={weather.main.temp}
+        // min={weather.main.temp_min}
+        // max={weather.main.temp_max}
+      />
+    );
+  });
 
-      </WeatherCard>
-    )
-  }) 
   return (
-    <div>
-      <h2>{weatherData.name}</h2>
-      <h3>Weather Today</h3>
-      <span>
-        <ul>{weatherData?.weather[0].description}
-        <li>{weatherData.main?.temp}</li>
-        <li>{weatherData.main?.temp_min}</li>
-        <li>{weatherData.main?.temp_max}</li>
-        </ul>
-      </span>
-      
-    </div>
+    <>
+      <div>
+        <h2>{weatherData.name}</h2>
+        <h3>Weather Today</h3>
+        <span>
+          <ul>
+            { weatherDetails }
+          </ul>
+        </span>
+        
+      </div>
+    </>
   )
+    
 }
