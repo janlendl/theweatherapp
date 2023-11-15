53af6880f5fa826cca69468e8b70d970d96a86ea
Lendl Peralta <janlendl@gmail.com>
2023-11-15 11:41:32 -0700

    Set JSON stringify to weatherdata hook. Add temp details on component

--- src/components/WeatherInfo.jsx
+++ src/components/WeatherInfo.jsx
@@ -2,8 +2,6 @@ import React, {useEffect, useState} from 'react'
 import axios from 'axios';
 
 export default function WeatherInfo(props) {
-  //const {city} = props;
-  console.log('::CITY:: ', props.city);
 
   const [weatherData, setWeatherData] = useState({});
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=743366476b7670d23d2788f10c157d96`;
@@ -12,7 +10,7 @@ export default function WeatherInfo(props) {
     if (props.city) {
       axios.get(url)
       .then((res) => {
-        setWeatherData(res.data);
+        JSON.stringify(setWeatherData(res.data));
         console.log('Weather API call:: ', res.data);
       })
       .catch((err) => {
@@ -21,11 +19,19 @@ export default function WeatherInfo(props) {
     }
   },[props.city, url]);
     
+  //const celcius = ;
   return (
     <div>
-      <h2>City selected: {props.city}</h2>
-      <h3>Weather Today is</h3>
-      {weatherData.name}
+      <h2>{weatherData.name}</h2>
+      <h3>Weather Today</h3>
+      <span>
+        <ul>{weatherData?.weather[0].description}
+        <li>{weatherData.main?.temp}</li>
+        <li>{weatherData.main?.temp_min}</li>
+        <li>{weatherData.main?.temp_max}</li>
+        </ul>
+      </span>
+      
     </div>
   )
 }
