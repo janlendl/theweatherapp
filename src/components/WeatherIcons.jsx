379c5b9e6e02676c99aa2eb32264d4a2b1a69f8c
Lendl Peralta <janlendl@gmail.com>
2023-12-01 21:54:53 -0700

    Import additional svg icon files

--- src/components/WeatherIcons.jsx
+++ src/components/WeatherIcons.jsx
@@ -8,6 +8,16 @@ import FewCloudsN from 'public/icons/few_clouds_night.svg';
 import FewClouds from 'public/icons/few_clouds.svg';
 import MistN from 'public/icons/mist_night.svg';
 import Mist from 'public/icons/mist.svg';
+import RainN from 'public/icons/rain_night.svg';
+import Rain from 'public/icons/rain.svg';
+import ScatteredCloudsN from 'public/icons/scattered_clouds_night.svg';
+import ScatteredClouds from 'public/icons/scattered_clouds.svg';
+import ShowerRainN from 'public/icons/shower_rain_night.svg';
+import ShowerRain from 'public/icons/shower_rain.svg';
+import SnowN from 'public/icons/snow_night.svg';
+import Snow from 'public/icons/snow.svg';
+import ThunderstormN from 'public/icons/thunderstorn_night.svg';
+import Thunderstorm from 'public/icons/thunderstorn_night.svg';
 
 
 export default function WeatherIcons() {
