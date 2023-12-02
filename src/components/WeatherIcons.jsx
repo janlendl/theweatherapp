a544a807855718f2376330d965f38eb4b8527e6e
Lendl Peralta <janlendl@gmail.com>
2023-12-01 21:52:15 -0700

    Add import files

---
+++ src/components/WeatherIcons.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import BrokenCloudsN from 'public/icons/broken_clouds_night.svg';
+import BrokenClouds from 'public/icons/broken_clouds.svg';
+import ClearN from 'public/icons/clear_night.svg';
+import Clear from 'public/icons/clear.svg';
+import FewCloudsN from 'public/icons/few_clouds_night.svg';
+import FewClouds from 'public/icons/few_clouds.svg';
+import MistN from 'public/icons/mist_night.svg';
+import Mist from 'public/icons/mist.svg';
+
+
+export default function WeatherIcons() {
+  return (
+    <div>WeatherIcons</div>
+  )
+}
