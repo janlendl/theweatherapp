bee1b4b2e539c0aba3c661b20333b8f56facf040
Lendl Peralta <janlendl@gmail.com>
2023-12-01 22:35:02 -0700

    Fix files

--- src/components/WeatherInfo.jsx
+++ src/components/WeatherInfo.jsx
@@ -2,28 +2,29 @@ import React, {useEffect, useState} from 'react'
 import axios from 'axios';
 
 import {convertToCelcius} from '../helpers/converter';
+import WeatherIcons from './WeatherIcons';
 import './WeatherInfo.scss';
 
-const icons = {
-  "01d": "public/icons/clear.svg",
-  "01n": "public/icons/clear_night.svg",
-  "02d": "public/icons/few_clouds.svg",
-  "02n": "public/icons/few_clouds_night.svg",
-  "03d": "public/icons/scattered_clouds.svg",
-  "03n": "public/icons/scattered_clouds_night.svg",
-  "04d": "public/icons/broken_clouds.svg",
-  "04n": "public/icons/broken_clouds_night.svg",
-  "09d": "public/icons/shower_rain.svg",
-  "09n": "public/icons/shower_rain_night.svg",
-  "10d": "public/icons/rain.svg",
-  "10n": "public/icons/rain_night.svg",
-  "11d": "public/icons/thunderstorm.svg",
-  "11n": "public/icons/thunderstorn_night.svg",
-  "13d": "public/icons/snow.svg",
-  "13n": "public/icons/snow_night.svg",
-  "50d": "public/icons/mist.svg",
-  "50n": "public/icons/mist_night.svg"
-}
+// const icons = {
+//   "01d": "public/icons/clear.svg",
+//   "01n": "public/icons/clear_night.svg",
+//   "02d": "public/icons/few_clouds.svg",
+//   "02n": "public/icons/few_clouds_night.svg",
+//   "03d": "public/icons/scattered_clouds.svg",
+//   "03n": "public/icons/scattered_clouds_night.svg",
+//   "04d": "public/icons/broken_clouds.svg",
+//   "04n": "public/icons/broken_clouds_night.svg",
+//   "09d": "public/icons/shower_rain.svg",
+//   "09n": "public/icons/shower_rain_night.svg",
+//   "10d": "public/icons/rain.svg",
+//   "10n": "public/icons/rain_night.svg",
+//   "11d": "public/icons/thunderstorm.svg",
+//   "11n": "public/icons/thunderstorn_night.svg",
+//   "13d": "public/icons/snow.svg",
+//   "13n": "public/icons/snow_night.svg",
+//   "50d": "public/icons/mist.svg",
+//   "50n": "public/icons/mist_night.svg"
+// }
 
 export default function WeatherInfo(props) {
 
@@ -52,7 +53,7 @@ export default function WeatherInfo(props) {
           <h2>{weatherData.name}</h2>
           <div className='weatherIcon'>
             {/* <img src={`https://openweathermap.org/img/wn/${weatherData.weather?.icon}.png`} alt='icon' /> */}
-            <img src={icons[weatherData.weather?.icon]} alt='icon' />
+            <WeatherIcons>{weatherData.weather?.icon}</WeatherIcons>
           </div>
           <div className='weatherDescription'>
             <span>{weatherData.weather?.description}</span>
