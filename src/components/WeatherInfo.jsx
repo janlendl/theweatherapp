a4e4db6a1103c43cee58b8cea4f3a5aa57a00e4d
Lendl Peralta <janlendl@gmail.com>
2024-01-04 21:19:48 -0700

    Clean up commented code

--- src/components/WeatherInfo.jsx
+++ src/components/WeatherInfo.jsx
@@ -5,27 +5,6 @@ import {convertToCelcius} from '../helpers/converter';
 import WeatherIcons from './WeatherIcons';
 import './WeatherInfo.scss';
 
-// const icons = {
-//   "01d": "public/icons/clear.svg",
-//   "01n": "public/icons/clear_night.svg",
-//   "02d": "public/icons/few_clouds.svg",
-//   "02n": "public/icons/few_clouds_night.svg",
-//   "03d": "public/icons/scattered_clouds.svg",
-//   "03n": "public/icons/scattered_clouds_night.svg",
-//   "04d": "public/icons/broken_clouds.svg",
-//   "04n": "public/icons/broken_clouds_night.svg",
-//   "09d": "public/icons/shower_rain.svg",
-//   "09n": "public/icons/shower_rain_night.svg",
-//   "10d": "public/icons/rain.svg",
-//   "10n": "public/icons/rain_night.svg",
-//   "11d": "public/icons/thunderstorm.svg",
-//   "11n": "public/icons/thunderstorn_night.svg",
-//   "13d": "public/icons/snow.svg",
-//   "13n": "public/icons/snow_night.svg",
-//   "50d": "public/icons/mist.svg",
-//   "50n": "public/icons/mist_night.svg"
-// }
-
 export default function WeatherInfo(props) {
 
   const [weatherData, setWeatherData] = useState({});
